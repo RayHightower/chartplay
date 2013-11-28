e07cfc0883957be8223cebb0fabd0c043fde7c1f
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-28 08:59:36 -0600

    build the window and refer to the chart view controllers

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,20 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = UITabBarController.alloc.init
+    @window.rootViewController.wantsFullScreenLayout = true
+
+    chart_view_controllers = [
+      PieChartViewController
+    ]
+
+    chart_view_controller_instances = chart_view_controllers.map do |chart_view_controller|
+      chart_view_controller.alloc.init
+    end
+
+    @window.rootViewController.viewControllers = chart_view_controller_instances
+
+    @window.makeKeyAndVisible
     true
   end
 end
