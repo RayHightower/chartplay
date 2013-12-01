11eb034614628bbb08e89a872ace42d8878b8956
Raymond T. Hightower <rth@wisdomgroup.com>
2013-12-01 14:56:20 -0600

    add a line chart view controller

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -6,7 +6,8 @@ class AppDelegate
 
     chart_view_controllers = [
       # BarChartViewController,
-      PieChartViewController
+      PieChartViewController,
+      LineChartViewController
     ]
 
     chart_view_controller_instances = chart_view_controllers.map do |chart_view_controller|
